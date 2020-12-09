e05e3264e5078d0cf3d345e85395ca313d6f6632
mlaizure <maddi.laizure@gmail.com>
2020-12-09 01:20:09 +0000

    Updated NULL head check

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,7 +12,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *p = *head;
 
-	if (!*head)
+	if (!head || !*head)
 		return (NULL);
 
 	if (!new)
