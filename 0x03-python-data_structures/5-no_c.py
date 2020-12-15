f9caffe98c18d0ce2041695fcbb55978f98307bb
mlaizure <maddi.laizure@gmail.com>
2020-12-15 23:19:10 +0000

    Changed find element logic

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
     if my_string:
         my_list = list(my_string)
-        for i in my_list:
-            if i == 'c':
-                my_list.remove('c')
-            if i == 'C':
-                my_list.remove('C')
-        return new_str.join(my_list)
+        while 'c' in my_list:
+            my_list.remove('c')
+        while 'C' in my_list:
+            my_list.remove('C')
+        return "".join(my_list)
     else:
-        return new_str
+        return my_string
