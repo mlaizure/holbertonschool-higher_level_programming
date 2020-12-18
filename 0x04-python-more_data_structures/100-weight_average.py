ea1d86d1304171dc723bbb53145a3e9ec53f87d1
mlaizure <maddi.laizure@gmail.com>
2020-12-18 01:34:22 +0000

    Added empty list logic

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     top = sum([x * y for x, y in my_list])
     bottom = sum([y for x, y in my_list])
     return top/bottom
