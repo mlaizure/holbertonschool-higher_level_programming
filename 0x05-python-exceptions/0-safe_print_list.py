e977435ce56a363533e1830ff84eb581b773fdca
mlaizure <maddi.laizure@gmail.com>
2021-01-04 19:31:32 +0000

    Changed loop parameters

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num_printed = 0
-    for idx, element in enumerate(my_list):
-        if idx < x:
-            try:
-                print(element, end="")
-            except IndexError:
-                pass
-            else:
-                num_printed += 1
+    for idx in range(x):
+        try:
+            print(my_list[idx], end="")
+        except IndexError:
+            pass
+        else:
+            num_printed += 1
     print()
     return num_printed
