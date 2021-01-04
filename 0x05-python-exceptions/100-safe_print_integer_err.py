91c01b9ae32b41ea5ed76e38ab7740494f2ea820
mlaizure <maddi.laizure@gmail.com>
2021-01-04 21:40:56 +0000

    Fixed pep8 error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        print("Exception: {}".format(err), file = sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
     return True
