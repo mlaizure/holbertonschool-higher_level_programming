0c478577e170de9731af8599f706350a48bea924
mlaizure <maddi.laizure@gmail.com>
2021-01-07 23:34:14 +0000

    Task 0 and test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module has a function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """adds integers a and b, casts floats as ints, b defatuls to 98"""
+    if not isinstance(a, (float, int)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (float, int)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
