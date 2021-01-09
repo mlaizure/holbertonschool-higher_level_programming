46100120009c8aff3bb19494169dffb22add2861
mlaizure <maddi.laizure@gmail.com>
2021-01-09 06:25:21 +0000

    Changed ' to \"

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     """adds integers a and b, casts floats as ints, b defatuls to 98"""
     if not isinstance(a, (float, int)):
-        raise TypeError('a must be an integer')
+        raise TypeError("a must be an integer")
     if not isinstance(b, (float, int)):
-        raise TypeError('b must be an integer')
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
