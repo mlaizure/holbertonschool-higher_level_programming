c4ad6be3f84c208b89cf7740725b6b5ecf3b4464
mlaizure <maddi.laizure@gmail.com>
2021-01-13 01:11:39 +0000

    Task 31

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module with LockedClass"""
+
+
+class LockedClass:
+    """prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name"""
+    __slots__ = ['first_name']
