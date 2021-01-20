57c6c6a73d97e2dc8d070366504c4c20d4fdcc6b
mlaizure <maddi.laizure@gmail.com>
2021-01-20 00:41:06 +0000

    Imported superclass instead of copying

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """Module with a class Rectangle that inherits from class BaseGeometry"""
 
-
-class BaseGeometry:
-    """public methods area and int validator"""
-    def area(self):
-        """area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """makes sure value is an int and greater than zero"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
