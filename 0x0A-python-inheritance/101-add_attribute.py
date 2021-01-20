c4697556e9f59d525433979743899ebc16dd1ff0
mlaizure <maddi.laizure@gmail.com>
2021-01-20 01:51:01 +0000

    Deleted main

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,20 +8,3 @@ def add_attribute(obj, name, value):
         setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
-
-if __name__ == "__main__":
-    add_attribute = __import__('101-add_attribute').add_attribute
-
-    class MyClass():
-        pass
-
-    mc = MyClass()
-    add_attribute(mc, "name", "John")
-    print(mc.name)
-
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
