423b51c4dea279d1588785a882a7d82c2a4f7bf4
mlaizure <maddi.laizure@gmail.com>
2021-01-20 00:45:40 +0000

    Imported superclass instead of copying

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,36 +2,7 @@
 """Module with a class Rectangle that inherits from class BaseGeometry"""
 
 
-class BaseGeometry:
-    """public methods area and int validator"""
-    def area(self):
-        """area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """makes sure value is an int and greater than zero"""
-        if type(value) != int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """instantiates with width and height validated by int validator in
-    BaseGeometry class, area and __str__ methods"""
-    def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """calculates area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """returns formatted string to print"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
