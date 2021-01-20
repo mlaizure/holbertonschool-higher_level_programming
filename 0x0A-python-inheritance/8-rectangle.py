38f471099907de5d13fb3831f6e098259ec31ef0
mlaizure <maddi.laizure@gmail.com>
2021-01-20 00:08:33 +0000

    Fixed init

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,8 +20,10 @@ class Rectangle(BaseGeometry):
     """instantiates with width and height validated by int validator in
     BaseGeometry class"""
     def __init__(self, width, height):
-        self.__width = BaseGeometry.integer_validator(self, "width", width)
-        self.__height = BaseGeometry.integer_validator(self, "height", height)
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
 
 if __name__ == "__main__":
     Rectangle = __import__('8-rectangle').Rectangle
