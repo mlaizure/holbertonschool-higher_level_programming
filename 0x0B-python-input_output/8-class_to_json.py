e75628ac8154d5257357e9dd7874d5f3423a4283
mlaizure <maddi.laizure@gmail.com>
2021-01-21 00:07:02 +0000

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with function that returns the dictionary description with simple
+data structure for JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """function that returns the dictionary description with simple data
+    structure for JSON serialization of an object"""
+    return obj.__dict__
