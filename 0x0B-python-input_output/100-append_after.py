edcd66febcbf518be38fda28d9d357579dd4aea1
mlaizure <maddi.laizure@gmail.com>
2021-01-21 21:18:01 +0000

    Moved code inside with

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,11 +6,13 @@ line containing a specific string"""
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text to a file, after each line containing a
     specific string"""
-    with open(filename, mode='r', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         contents = f.readlines()
-    for idx, line in enumerate(contents):
-        if search_string in line:
-            contents.insert(idx + 1, new_string)
-    contents = "".join(contents)
+
+        for idx, line in enumerate(contents):
+            if search_string in line:
+                contents.insert(idx + 1, new_string)
+        contents = "".join(contents)
+
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(contents)
