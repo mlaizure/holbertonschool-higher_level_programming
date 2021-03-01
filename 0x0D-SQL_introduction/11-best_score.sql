eda6d48f043202f2f967fe1ac235fd6837b03034
mlaizure <maddi.laizure@gmail.com>
2021-03-01 22:38:23 +0000

    Task 11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- score >= 10 in the table second_table of the database hbtn_0c_0
+-- display score and name, highest score first
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
