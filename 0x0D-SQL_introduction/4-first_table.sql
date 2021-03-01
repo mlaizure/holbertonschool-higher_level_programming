a91ddcad74df58ccfe02dcc8917f95077d898411
mlaizure <maddi.laizure@gmail.com>
2021-03-01 21:29:40 +0000

    Task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates table called first_table in current database in your MySQL server
+-- has id INT and name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
