c3fa6410b82cea80ba8c2c7c7249bf5a1c69f08c
mlaizure <maddi.laizure@gmail.com>
2021-03-02 21:59:26 +0000

    Task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- display cities.id - cities.name - states.name
+SELECT cities.id, cities.name, states.name FROM cities, states
+WHERE cities.state_id = states. id ORDER BY cities.id;
