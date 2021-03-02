233200d6a1810a985f08ebdf2148ab4c5c70cb10
mlaizure <maddi.laizure@gmail.com>
2021-03-02 23:13:29 +0000

    Task 11 and fixed typo on 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows in hbtn_0d_tvshows that have at least one genre linked
--- displays tv_shows.title - tv_show_genres.genre.id
+-- displays tv_shows.title - tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
