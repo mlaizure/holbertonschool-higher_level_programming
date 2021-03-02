580d6b41dac557fbfdd16a2e5b9f86e853c479c7
mlaizure <maddi.laizure@gmail.com>
2021-03-02 21:01:49 +0000

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- creates database hbtn_0d_usa and table cities (in database hbtn_0d_usa)
+-- city has int primary key, name can't be null, foreign key state_id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+name VARCHAR(256) NOT NULL);
