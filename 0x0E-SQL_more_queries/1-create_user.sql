99f58fb8985a024dd7dea5427561f30492dd54d0
mlaizure <maddi.laizure@gmail.com>
2021-03-02 20:28:34 +0000

    Added privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- has all privileges, password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
