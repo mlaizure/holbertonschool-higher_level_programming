b40942189f6c70edad95bd9ecc206175e8d15015
mlaizure <maddi.laizure@gmail.com>
2021-03-03 00:47:12 +0000

    Added order by

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,5 @@
 -- displays tv_genres.name
 SELECT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres
 WHERE tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id
-AND tv_genres.id = tv_show_genres.genre_id;
+AND tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_genres.name ASC;
