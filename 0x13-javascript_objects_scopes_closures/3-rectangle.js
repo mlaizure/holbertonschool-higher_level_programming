05f2b5fae03d610fad7cf7414bb11118097a4988
mlaizure <maddi.laizure@gmail.com>
2021-03-23 18:38:19 +0000

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!w || !h || w <= 0 || h <= 0) {
+      // pass
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
