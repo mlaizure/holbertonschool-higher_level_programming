92ae69ccf72bea306fee15fdbde1023439f285e2
mlaizure <maddi.laizure@gmail.com>
2021-03-23 22:03:13 +0000

    Updated map logic

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const list1 = list.map(x => x * list.indexOf(x));
+const list1 = list.map((value, idx) => value * idx);
 console.log(list1);
