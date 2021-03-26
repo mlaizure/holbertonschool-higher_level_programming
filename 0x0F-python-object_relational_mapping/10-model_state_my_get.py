068f52c1237e2c9ff6da5bc592eb97bdb1ea0fc2
mlaizure <maddi.laizure@gmail.com>
2021-03-26 02:38:10 +0000

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists all State objects that contain 'a' from the database
+hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user_name, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
