ecafe19974021af57858b794ae18948f2e3ba3bd
mlaizure <maddi.laizure@gmail.com>
2021-03-27 21:07:54 +0000

    Allowed to fetch all instead of one

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name LIKE BINARY
     '{}' ORDER BY id ASC""".format(state_searched))
-    print(cur.fetchone())
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     db.close()
