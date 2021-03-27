e7398f477644d77917875fe22e2a468a158ab671
mlaizure <maddi.laizure@gmail.com>
2021-03-27 21:11:04 +0000

    Allowed to fetch all instead of one

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=user_name,
                          passwd=password, db=database_name, charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                (state_searched,))
-    print(cur.fetchone())
+    cur.execute("""SELECT * FROM states WHERE name = %s
+    ORDER BY id ASC""",(state_searched,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     db.close()
