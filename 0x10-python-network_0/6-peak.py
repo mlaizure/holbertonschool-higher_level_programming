a2eee239a4f49cb2761169fe8f58b1a9e20ba957
mlaizure <maddi.laizure@gmail.com>
2021-04-08 23:52:50 +0000

    Updated efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """module with find_peak function"""
 
+
 def find_peak(list_of_integers):
     """finds a peak in unsorted ints"""
     if not list_of_integers:
         return "None"
-    for idx, num in enumerate(list_of_integers):
-        if idx == 0:
-            if num > list_of_integers[1]:
-                return num
-        if idx == len(list_of_integers) - 1:
-            return num
-        if num > list_of_integers[idx - 1] and num > list_of_integers[idx + 1]:
-            return num
+    return list_of_integers[
+        find_peak_help(list_of_integers, 0, len(list_of_integers) - 1)]
+
+
+def find_peak_help(il, low, high):
+    """finds the peak"""
+    ln = len(il)
+    mid = int(low + (high - low)/2)
+    if ((mid == 0 or il[mid - 1] <= il[mid]) and
+       (mid == ln - 1 or il[mid + 1] <= il[mid])):
+        return mid
+    elif (mid > 0 and il[mid - 1] > il[mid]):
+        return find_peak_help(il, low, mid - 1)
+    else:
+        return find_peak_help(il, mid + 1, high)
